1c4005bff4bb2fa502c7992fbfc458cb996aaae7
lukasz.czaplinski <lukasz.czaplinski@tooploox.com>
2018-09-28 14:04:35 +0200

    Add state to App component

--- src/common/App/App.spec.tsx
+++ src/common/App/App.spec.tsx
@@ -6,8 +6,5 @@ describe("App", () => {
   it("adds bang after making a bang", () => {
     const tree = shallow(<App />);
     expect(tree.find("h1").text()).toEqual("Hello, World!");
-    tree.find("#bang").simulate("click");
-    tree.find("#bang").simulate("click");
-    expect(tree.find("h1").text()).toEqual("Hello, World!!!");
   });
 });
--- src/common/App/App.tsx
+++ src/common/App/App.tsx
@@ -3,26 +3,98 @@ import React, { Component } from "react";
 
 const cls = require("./App.css");
 
-export class App extends Component<{}, { text: string }> {
+type ResourceValue = number;
+
+interface Resources {
+  money: ResourceValue;
+  popularity: ResourceValue;
+}
+
+// Value from 0 to 100 in percents
+type PowerCenterValue = number;
+interface PowerCenters {
+  media: PowerCenterValue;
+  legislation: PowerCenterValue;
+  military: PowerCenterValue;
+  judiciary: PowerCenterValue;
+}
+
+type ID = string;
+
+enum ResourceEffectType {
+  ChangeResource = "ChangeResource",
+}
+
+enum PowerCenterEffectType {
+  ChangePowerCenter = "ChangePowerCenter",
+}
+
+enum ResourceEffectTarget {
+  Money = "money",
+  Popularity = "popularity",
+}
+
+enum PowerCenterEffectTarget {
+  Media = "media",
+  Legislation = "legislation",
+  Military = "military",
+  Judiciary = "judiciary",
+}
+
+interface ResourceEffect {
+  type: ResourceEffectType;
+  target: ResourceEffectTarget;
+  value: ResourceValue;
+}
+
+interface PowerCenterEffect {
+  type: PowerCenterEffectType;
+  target: PowerCenterEffectTarget;
+  value: PowerCenterValue;
+}
+
+type Effect = ResourceEffect | PowerCenterEffect;
+
+interface Choice {
+  id: ID;
+  description: string;
+  label: string;
+  effects: Effect[];
+}
+interface Decision {
+  id: ID;
+  description: string;
+  deadline: Date;
+  choices: Choice[];
+}
+
+interface AppState {
+    resources: Resources;
+    powerCenters: PowerCenters;
+    decisions: Decision[];
+}
+
+export class App extends Component<{}, AppState> {
   public state = {
-    text: "Hello, World!",
+    resources: {
+      money: 0,
+      popularity: 0,
+    },
+    powerCenters: {
+      military: 0,
+      judiciary: 0,
+      legislation: 0,
+      media: 0,
+    },
+    decisions: [],
   };
 
-  public bang = () => {
-    this.setState(({ text }) => ({ text: `${text}!` }));
-  }
-
   public render() {
-    const { text } = this.state;
-
     return (
       <div>
         <h1 className={cls.title}>
-          {text}
+          Hello, World!
         </h1>
-        <button onClick={this.bang} id="bang" type="button">
-          Bang!
-        </button>
       </div>
     );
   }
